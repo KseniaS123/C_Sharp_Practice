5fccb1cde4202a8f8102b3f4cd5d82d22de47550
Мария <you@example.com>
2022-11-21 22:37:27 +0300

    Task 1_4

---
+++ 1_lesson/1_4/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу вычисления модуля числа.
+
+Console.WriteLine("Write a number: ");
+int a = int.Parse(Console.ReadLine());
+
+int b = Math.Abs(- a);
+Console.WriteLine($"{b}");
