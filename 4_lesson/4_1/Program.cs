9a3718a181cede5c4da89d5b235fe63f1480d417
Ксения <you@example.com>
2022-12-13 15:30:32 +0300

    Task 4_1

---
+++ 4_lesson/4_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// 1. Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+
+int NumbCount(int num)
+{
+    if(num==0)
+    {
+        return 1;
+    }
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+Console.WriteLine(NumbCount(int.Parse(Console.ReadLine())));
