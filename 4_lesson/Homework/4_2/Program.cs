1ea7e18b61db8e4187455174af599151c2b7a539
Ксения <you@example.com>
2022-12-13 20:28:13 +0300

    Homework task 2

---
+++ 4_lesson/Homework/4_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Summa(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + (num % 10);
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число: ");
+Console.WriteLine($"Сумма цифр в числе = {Summa(int.Parse(Console.ReadLine()))}");
