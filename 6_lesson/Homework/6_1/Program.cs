83f2c4dff7ba3cc18036ee658f37617c0636487b
Ксения <you@example.com>
2022-12-14 21:43:37 +0300

    Homework task 1

---
+++ 6_lesson/Homework/6_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int PosNum(int amount)
+{
+    int count = 0;
+
+    for (int i = 0; i < amount; i++)
+    {
+        Console.Write($"Введите число: ");
+        int num = int.Parse(Console.ReadLine());
+        if (num > 0)
+            count++;
+    }
+    return count;
+}
+
+Console.Write("Введите количecnво чисел: ");
+int amount = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество чисел больше 0: {PosNum(amount)}");
+
