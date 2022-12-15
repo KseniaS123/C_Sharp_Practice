55d88731beb0267ce0523c85d5b426080a01518c
Ксения <you@example.com>
2022-12-15 15:38:21 +0300

    Homework task 2, but need to edit

---
+++ 7_lesson/Homework/7_3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] Array(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int Average(int[,] mas)
+{
+    
+}
+
+Console.Write("Введите число строк: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите число столбцов: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = Array(row, column,
+                    int.Parse(Console.ReadLine()),
+                    int.Parse(Console.ReadLine()));
+Console.WriteLine();
+Print(arr_1);
+Console.WriteLine(FindElement(arr_1, int.Parse(Console.ReadLine()),
+                                    int.Parse(Console.ReadLine())));
