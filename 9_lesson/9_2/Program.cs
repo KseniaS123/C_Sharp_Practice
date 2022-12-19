a749c287aa0af1dea9bc139fd3f1b5900410608b
Ксения <you@example.com>
2022-12-19 21:12:31 +0300

    Task 9_2

---
+++ 9_lesson/9_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// 2. Задайте значения M и N. Напишите программу, которая 
+//    рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+
+void IntegersSecond(int M, int N)
+{
+    if (M > N) return;
+    Console.Write($"{M} ");
+    IntegersSecond(M + 1, N);
+}
+IntegersSecond(5, 10);
+Console.WriteLine();
