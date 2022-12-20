c5ac28e5f0c61e9ea425f68838d9325df99bfdb2
Ксения <you@example.com>
2022-12-20 10:42:00 +0300

    Homework task 9_1

---
+++ 9_lesson/Homework/9_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите программу,
+// которая выведет все четные натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "4, 2"
+// N = 8 -> "4, 6, 8"
+
+
+void Integers(int N)
+{
+    if (N == 0) return;
+    if (N % 2 == 0)
+        Console.Write($"{N} ");
+    Integers(N - 1);   
+}
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write($"Четные числа от {n} до 1: ");
+Integers(n);
+Console.WriteLine();
