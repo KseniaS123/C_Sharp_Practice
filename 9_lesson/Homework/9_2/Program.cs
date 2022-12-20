f59e1938baf59294495dc30e142b4f1b251a1cae
Ксения <you@example.com>
2022-12-20 12:36:18 +0300

    Homework task 9_2 change function name

--- 9_lesson/Homework/9_2/Program.cs
+++ 9_lesson/Homework/9_2/Program.cs
@@ -5,16 +5,16 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-int IntegersSecond(int M, int N)
+int IntegersSumma(int M, int N)
 {
     if (M > N) return 0;
     if (N == M) return M;
-    return M + IntegersSecond(M + 1, N);
+    return M + IntegersSumma(M + 1, N);
 }
 
 Console.WriteLine("Введите число M: ");
 int m = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите число N: ");
 int n = int.Parse(Console.ReadLine());
-Console.WriteLine($"Сумма чисел в промежутке от {m} до {n} = {IntegersSecond(m, n)}");
+Console.WriteLine($"Сумма чисел в промежутке от {m} до {n} = {IntegersSumma(m, n)}");
 
