7776cc35aa8dd7d9d598f6204fe0f6e5be818551
Ксения <you@example.com>
2022-12-20 12:16:11 +0300

    Homework task 9_2

---
+++ 9_lesson/Homework/9_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных
+// элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int IntegersSecond(int M, int N)
+{
+    if (M > N) return 0;
+    if (N == M) return M;
+    return M + IntegersSecond(M + 1, N);
+}
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма чисел в промежутке от {m} до {n} = {IntegersSecond(m, n)}");
+
